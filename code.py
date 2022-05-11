aad69dda7d442c5e3ff0a9e519e37a4caa2d3f1a
MahnoorFatima110 <71967454+MahnoorFatima110@users.noreply.github.com>
2022-05-11 22:23:11 +0500

    Add files via upload

---
+++ code.py
@@ -0,0 +1,20 @@
+
+import pandas as pd 
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split as tts
+from sklearn.linear_model import LinearRegression as LR
+from sklearn.metrics import r2_score
+
+dataset=pd.read_csv("E:/Ai Lab/project 5/dataset/dataset.csv")
+
+X=dataset.iloc[:,[0,1]].values
+Y=dataset.iloc[:,2:3].values
+
+X_train,X_test,Y_train,Y_test=tts(X,Y,train_size=0.6,random_state=1)
+
+
+
+model=LR()
+model.fit(X_train,Y_train)
+
+res=model.predict(X_test)
